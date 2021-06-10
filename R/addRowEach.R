c97cf95e1b783d2890d3ded0e9f422822324426b
CARTERON Paul <carteronpaul@gmail.com>
2021-06-10 15:33:44 +0200

    AddRowEach function

---
+++ R/addRowEach.R
@@ -0,0 +1,16 @@
+#' addRowEach
+#'
+#' @param data_frame Dataframe contenant les donnees du excel
+#' @param interval Nombre de lignes avant de rajouter une ligne vide
+#'
+#' @return
+#' @export
+#'
+addRowEach = function(data_frame, interval = 4){
+
+   cut <- rep(1:ceiling(nrow(data_frame)/interval), each = interval, len = nrow(data_frame))
+   data_frame = data_frame[Reduce(c, lapply(split(1:nrow(data_frame), cut), c, NA)), ]
+
+   return(data_frame)
+}
+
