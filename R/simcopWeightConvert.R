025274eccf54e79c25f3e579b9fc5fd50aa86419
CARTERON Paul <carteronpaul@gmail.com>
2021-07-01 10:32:18 +0200

    new function for writing ecl weight faster for simcop script

---
+++ R/simcopWeightConvert.R
@@ -0,0 +1,23 @@
+#' simcopWeightConvert
+#'
+#' @param ... number separate with ","
+#' @param nbClasse nombre de classe utilisees par eclaircie
+#'
+#' @return Renvoi les nombres formattes au mode script
+#' @export
+#'
+simcopWeightConvert = function(..., nbClasse = 5){
+
+   number = list(...)
+   number = unlist(number)
+
+   nbEcl = length(number)/nbClasse
+
+   multipleRes = split(number, rep_len(1:nbEcl, length(number)))
+
+   multipleRes = lapply(multipleRes, function(x){paste0("[",paste0(x,collapse = ","),"]")})
+   res = paste(unlist(multipleRes), collapse = "\n")
+
+   return(res = cat(res))
+
+}
