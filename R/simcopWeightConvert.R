9adbce0a876d56a653509822d758a8877afb0c00
CARTERON Paul <carteronpaul@gmail.com>
2021-07-01 11:02:23 +0200

    je suis un teubé

--- R/simcopWeightConvert.R
+++ R/simcopWeightConvert.R
@@ -12,8 +12,7 @@ simcopWeightConvert = function(..., nbClasse = 5){
    number = unlist(number)
 
    nbEcl = length(number)/nbClasse
-
-   multipleRes = split(number, rep_len(1:nbEcl, length(number)))
+   multipleRes = split(number, rep(1:nbEcl, each = nbClasse))
 
    multipleRes = lapply(multipleRes, function(x){paste0("[",paste0(x,collapse = ","),"]")})
    res = paste(unlist(multipleRes), collapse = "\n")
